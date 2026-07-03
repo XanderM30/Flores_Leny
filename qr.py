457864d0e18661f40b68875da3a3c070216e9476
Xander30 <alexanderalvaro742@gmail.com>
2026-07-03 00:14:23 -0600

    Primer versión campo de flores

--- qr.py
+++ qr.py
@@ -1,20 +1,15 @@
 import qrcode
 
-# 1. Define el enlace que quieres codificar
-link = "https://google.com"
+link = "https://xanderm30.github.io/Flores_Leny/"
 
-# 2. Genera el código QR
 qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=10,
-    border=4,
+    border=4
 )
+
 qr.add_data(link)
 qr.make(fit=True)
 
-# 3. Crea y guarda la imagen
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("codigo_qr.png")
-
-print("¡Código QR generado exitosamente como 'codigo_qr.png'!")
